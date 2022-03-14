3b067b28bad951bc1ffc65446131276ebf2105f1
Floreti Designs <workyg@protonmail.com>
2022-03-14 12:21:28 -0400

    adding view pages.

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,24 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
-    const navStyle = {
-        display: "flex",
-        justifyContent: "space-around",
-        border: "1px purple",
-        margin: "auto",
-        padding: "5px",
-        width: "88%",
-    };
+    // const navStyle = {
+    //     display: "flex",
+    //     justifyContent: "space-around",
+    //     border: "1px purple",
+    //     margin: "auto",
+    //     padding: "5px",
+    //     width: "88%",
+    // };
 
     return (
         <Header>
             <nav style={navStyle}>
                 <Link to="/">
-                    <div><span>Ozz</span> Shipping</div>
+                    <div>Navbar</div>
                 </Link>
-                {/* <Link to="/packages">
-          <div>Ship A package</div>
-        </Link> */}
                 <Link to="/user">
                     <div>Profile</div>
                 </Link>
---
+++ pages/Register.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { Link } from "react-router-dom"
+
+function Register(props) {
+    
+
+};
+
+export default Register;
